b4e6d9dbae6e8c64385e198cf468c23c99c814db
Irina Little <irina.benediktovich@gmail.com>
2015-12-14 16:43:07 +1300

    Generated with Initializr

---
+++ src/main/java/nz/ac/auckland/auth/AuthzPageApplication.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthzPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthzPageApplication.class, args);
+    }
+}
