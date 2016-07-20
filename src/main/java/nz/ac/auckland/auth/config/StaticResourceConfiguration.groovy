e53adb4d8299499a7e1d79c26ee14cfb53248422
Irina Little <irina.little@auckland.ac.nz>
2016-07-20 17:27:23 +1200

    Change error handling to capture broken requests and respond with message

---
+++ src/main/java/nz/ac/auckland/auth/config/StaticResourceConfiguration.groovy
@@ -0,0 +1,16 @@
+package nz.ac.auckland.auth.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		}
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+}
